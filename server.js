44871329a59be2b867eeb00b910e626fb1eaa0af
Richard Saville <richardasaville@gmail.com>
2017-07-01 10:41:22 +1000

    added functionality to store data in json file

--- server.js
+++ server.js
@@ -45,6 +45,15 @@ app.get('/scrape', function(req, res) {
 
         }
 
+        fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err) {
+
+            console.log('File successfully written! - Check your project directory for the output.json file');
+
+        });
+
+        // Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
+        res.send('Check your console!');
+
     });
 });
 
